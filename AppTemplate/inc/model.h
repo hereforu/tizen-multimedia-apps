45e8348e08384dd2f32acc7187ac065f74da36d8
droppu123 <gdalhj@hanmail.net>
2016-06-10 14:19:21 +0900

    enhace code
    fix #10

--- AppTemplate/inc/model.h
+++ AppTemplate/inc/model.h
@@ -8,6 +8,8 @@
 #ifndef MODEL_H_
 #define MODEL_H_
 
+#include <vector>
+#include <string>
 
 class Model{
 
@@ -23,12 +25,18 @@ public:
 	bool Load();
 
 
+	void AddSrcToList(const char* srcFileName);
+	const char* GetSrcFromList(int index);
+	int GetSizeOfList();
+
+
 protected:
 	virtual bool creatspecifics() = 0;
 	virtual void destroyspecifics() = 0;
 
 private:
 	bool m_created;
+	std::vector<std::string> m_SrcList;
 
 };
 
